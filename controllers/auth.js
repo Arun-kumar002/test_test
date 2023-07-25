f1fdc2f4a5948300624d315a7681f4359ea2ec34
Arun-kumar002 <arunsubramanian002@gmail.com>
2023-07-25 23:01:39 +0530

    dev

---
+++ controllers/auth.js
@@ -0,0 +1,111 @@
+import mongoose from "mongoose";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+    res.status(200).send("User has been created!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const signin = async (req, res, next) => {
+  try {
+    const { name, password } = req.body;
+
+    const user = await User.findOne({ name });
+    if (!user) return next(createError(404, "User not found!"));
+
+    // Assuming you have bcrypt or other method to check the password
+    // const isCorrect = await bcrypt.compare(password, user.password);
+    // if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password: _, ...userWithoutPassword } = user._doc;
+
+    console.log(token)
+    // Set the token in the 'access_token' cookie and send it to the client
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: true, 
+      sameSite: 'None', 
+      maxAge: 3600000, 
+      path: '/', 
+    });
+
+    res.status(200).json({ message: 'success', });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+// export const signin = async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({ name: req.body.name });
+//     if (!user) return next(createError(404, "User not found!"));
+
+//     // const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+//     // if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
+
+//     const token = jwt.sign({ id: user._id }, process.env.JWT);
+//     const { password, ...others } = user._doc;
+//     console.log(token);
+//     res
+//       .cookie("access_token", token, {
+//         path: "/",
+//       });
+
+//     // res.cookie('myCookie', 'Hello, World!', {
+//     //   maxAge: 60 * 60 * 1000,
+//     //   path: '/',
+//     //   sameSite: 'None',
+//     //   secure: true,
+//     // });
+
+//     res.status(200)
+//       .json({ message: 'success', others });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+export const googleAuth = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(user._doc);
+    } else {
+      const newUser = new User({
+        ...req.body,
+        fromGoogle: true,
+      });
+      const savedUser = await newUser.save();
+      const token = jwt.sign({ id: savedUser._id }, process.env.JWT);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(savedUser._doc);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
